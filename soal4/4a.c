d8402f08b86d87e10d1cc15df51d9eef7e37fa23
Amelia <ameliapuji2000@gmail.com>
2020-04-03 10:21:25 +0700

    Revisi Soal 4

--- soal4/4a.c
+++ soal4/4a.c
@@ -4,73 +4,31 @@
 #include<stdlib.h> 
 #include<sys/shm.h>
 #include<sys/stat.h>
+#include <sys/ipc.h>
 
 int matA[4][2], rA = 4, cA = 2; 
 int matB[2][5], rB = 2, cB = 5;
-int (*matC)[5];
+int *matC;
+int result[4][5];
 int step_i = 0, sum = 0;
-pthread_t threads[10], t1, t2; 
-// int* p1;
-// int* p2;
-int* p; 
-
-// void* addA(void * arg) {
-//     printf("Masukkan Matriks A : \n");
-//     for (int i = 0; i < 4; i++) { 
-// 		for (int j = 0; j < 2; j++) {
-// 	    	scanf("%d", &matA[i][j]); 
-//         }
-// 	}
-
-// 	printf("\nMatriks A\n");
-// 	for (int i = 0; i < 4; i++) { 
-// 		for (int j = 0; j < 2; j++) {
-// 	    	printf("%d ", matA[i][j]); 
-//         }
-//         printf("\n"); 
-// 	}
-//     printf("\n"); 
-
-// }
-
-// void* addB(void * arg) {
-//     printf("Masukkan Matriks B : \n");
-//     for (int i = 0; i < 2; i++) { 
-// 		for (int j = 0; j < 5; j++) {
-// 	    	scanf("%d", &matB[i][j]); 
-//         }
-// 	}
-
-// 	printf("\nMatriks B\n");
-// 	for (int i = 0; i < 2; i++) { 
-// 		for (int j = 0; j < 5; j++) {
-// 	    	printf("%d ", matB[i][j]); 
-//         }
-//         printf("\n"); 
-// 	}
-// }
 
 void* multi(void* arg) 
 { 
     int core = step_i++; 
-  
-    // Each thread computes 1/4th of matrix multiplication 
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 5; j++) {  
-            for (int k = 0; k < 2; k++) {  
-                sum = sum + matA[i][k] * matB[k][j];
-            }
-            matC[i][j] = sum;
-            sum = 0;
-        }
-    } 
+    for (int i = core * 4 / 4; i < (core + 1) * 4 / 4; i++)  
+        for (int j = 0; j < 5; j++)  
+            for (int k = 0; k < 2; k++)  
+                result[i][j] += matA[i][k] * matB[k][j]; 
+    
 } 
 
 int main(void) 
 { 
-    key_t key = 12345;
-    int shmid = shmget(key, sizeof(int[5][5]), IPC_CREAT | 0666);
-    matC = shmat(shmid, 0, 0);
+    int rC = 4, cC = 5;
+
+    key_t key = 1234;
+    int shmid = shmget(key, sizeof(int)*rC*cC, IPC_CREAT | 0666);
+    matC = (int *)shmat(shmid, NULL, 0);
 
     matA[0][0] = 1;
 	matA[0][1] = 2;
@@ -84,28 +42,33 @@ int main(void)
 	matB[0][0] = 2;
 	matB[0][1] = 1;
 	matB[0][2] = 3;
-	matB[0][3] = 2;
+	matB[0][3] = 4;
 	matB[0][4] = 1;
 	matB[1][0] = 2;
 	matB[1][1] = 3;
 	matB[1][2] = 2;
 	matB[1][3] = 1;
 	matB[1][4] = 2;
-
-	// pthread_create(&t1, NULL, addA, (void*)(p1)); 
-    // pthread_join(t1, NULL);  
-    // pthread_create(&t2, NULL, addB, (void*)(p2));
-    // pthread_join(t2, NULL);     
+    
+    pthread_t threads[4]; 
   
-    for (int i = 0; i < 4; i++) pthread_create(&threads[i], NULL, multi, (void*)(p)); 
-  
-    for (int i = 0; i < 4; i++)  pthread_join(threads[i], NULL);     
-
-    printf("\nPerkalian Matriks A dan Matriks B : \n");
     for (int i = 0; i < 4; i++) { 
-        for (int j = 0; j < 5; j++)  {
-            printf("%d ", matC[i][j]); 
+        int* p; 
+        pthread_create(&threads[i], NULL, multi, (void*)(p)); 
+    } 
+  
+    for (int i = 0; i < 4; i++)  
+        pthread_join(threads[i], NULL);     
+  
+    printf("Multiplication of A and B : \n");
+    for (int i = 0; i < rC; i++) { 
+        for (int j = 0; j < cC; j++)  {
+            matC[i*cC+j] = result[i][j];
+            printf("%d ", matC[i*cC+j]);
         }
         printf("\n");
     } 
+
+    shmdt(matC);
+    shmctl(shmid, IPC_RMID, NULL);
 } 
--- soal4/4b.c
+++ soal4/4b.c
@@ -1,55 +1,58 @@
+
 #include<stdio.h> 
 #include<pthread.h> 
 #include<unistd.h> 
 #include<stdlib.h> 
 #include<sys/shm.h>
 #include<sys/stat.h>
-#include<tgmath.h>
+#include <sys/ipc.h>
 
-unsigned long long facto[4][5], arr[5][5];
-int rC = 4, cC = 5, res, result;
+int arr[5][5];
+int res, result;
 
-unsigned long long fact(unsigned long long n){
-    if(n==0 || n==1) return 1;
-    else return n* fact(n-1);
-}
 
-void *factorial_(void *arg){
-    for (int j = 0; j < 4; j++)  {
-        for (int k = 0; k < 5; k++) {  
-            printf("%llu\t\t\t", fact(arr[j][k]));
+void *plus(void *arg){
+    printf("\nMatriks Penambahan : \n");
+
+    for (int i = 0; i < 4; i++)  {
+        for (int j = 0; j < 5; j++) {  
+            int n = arr[i][j];
+            for (int k = 1; k <= n; k ++) {
+                res += k;
+            }
+            printf("%d ", res);
         }
         printf("\n");
     }
     pthread_exit(0);
-
 }
 
 int main() {
-    key_t key = 12345;
-    int (*matC)[5];
+    int rC = 4, cC = 5;
+    int *matC;
 
-    int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-    matC = shmat(shmid, 0, 0);
+    key_t key = 1234;
+    int shmid = shmget(key, sizeof(int)*rC*cC, IPC_CREAT | 0666);
+    matC = (int *)shmat(shmid, NULL, 0);
 
     printf("Perkalian Matriks A dan Matriks B : \n");
-    for (int i = 0; i < 4; i++) { 
-        for (int j = 0; j < 5; j++)  {
-            printf("%d ", matC[i][j]); 
+    for (int i = 0; i < rC; i++) { 
+        for (int j = 0; j < cC; j++)  {
+            printf("%d ", matC[i*cC+j]); 
+            arr[i][j] = matC[i*cC+j];
         }
         printf("\n");
     } 
 
-    for (int i = 0; i < 4; i++) { 
-        for (int j = 0; j < 5; j++)  {
-            arr[i][j] = matC[i][j]; 
-        }
-    } 
-
     pthread_t tid;
-    pthread_attr_t attr; // set of thread attributes
+    pthread_attr_t attr;
 
     pthread_attr_init(&attr);
-    pthread_create(&tid, &attr, factorial_, NULL);
+    pthread_create(&tid, &attr, plus, NULL);
     pthread_join(tid, NULL); 
+
+    shmdt(matC);
+    shmctl(shmid, IPC_RMID, NULL);
+
+    return 0;
 }
